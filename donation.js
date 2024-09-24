90587a7fce782db2a82737eea16d13b036500f94
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-09-24 21:08:32 +0530

    issues

--- donation.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("home").addEventListener("click", function () {
-  console.log("target success");
-  window.location.href = "./index.html";
-});
-console.log("im connected");
